0d8737b7f2ad7d0b910bcdcbf89bf1e49ff6f314
Steve <spete008@gmail.com>
2023-05-23 21:41:53 -0700

    ---

--- freemocap/gui/qt/widgets/sensor_widget.py
+++ freemocap/gui/qt/widgets/sensor_widget.py
@@ -1,4 +1,4 @@
-from PyQt6.QtWidgets import QLabel, QVBoxLayout, QWidget, QPushButton
+from PyQt6.QtWidgets import QLabel, QVBoxLayout, QWidget, QPushButton, QComboBox
 from PyQt6.QtCore import QTimer
 import serial
 import serial.tools.list_ports as list_ports
@@ -20,33 +20,49 @@ class SensorWidget(QWidget):
         self.elapsed_time = 0.0
         self.start_time = 0.0
 
+        # Create a combo box for selecting the COM port
+        self.com_port_combo = QComboBox()
+        self.layout.addWidget(self.com_port_combo)
+
         ports = list(list_ports.comports())
-        if ports:
-            port_name = ports[0].device
+        for port in ports:
+            self.com_port_combo.addItem(port.device)
+
+        self.com_port_combo.setCurrentIndex(0)  # Set the default selection to the first port
+
+        def get_selected_port():
+            return self.com_port_combo.currentText()
+
+        def open_serial_port():
+            port_name = get_selected_port()
             serialPort = serial.Serial(port=port_name, baudrate=115200, timeout=1)
-            self.chart_data = pd.DataFrame()
+            return serialPort
 
-            # Create a PlotWidget for real-time data plotting
-            self.plot_widget = pg.PlotWidget()
-            self.layout.addWidget(self.plot_widget)
+        def is_serial_port_open(serial_port):
+            return serial_port.isOpen()
 
-            # Initialize the plot curve
-            self.plot_curve = self.plot_widget.plot()
+        self.serial_port = open_serial_port()
+        self.chart_data = pd.DataFrame()
 
-            def collect_data():
-                serialPort.write(b'g')
-                time.sleep(0.1)
-                arduino_data = serialPort.readline().decode('ascii').split()
+        # Create a PlotWidget for real-time data plotting
+        self.plot_widget = pg.PlotWidget()
+        self.layout.addWidget(self.plot_widget)
+
+        # Initialize the plot curve
+        self.plot_curve = self.plot_widget.plot()
+
+        def collect_data():
+            if is_serial_port_open(self.serial_port):
+                self.serial_port.write(b'g')
+                time.sleep(0.05)
+                arduino_data = self.serial_port.readline().decode('ascii').split()
 
                 if arduino_data:
-                    print('arduino data' + arduino_data[0])
                     time_now = dt.datetime.now()
-                    #self.elapsed_time = time_now - self.start_time
                     self.elapsed_time = round((time_now - self.start_time).total_seconds(), 2)
-                    
+
                     new_row = {'time': time_now, 'kg': arduino_data[0], 'elapsedTime': self.elapsed_time}
                     new_df = pd.DataFrame([new_row])
-                    print(new_df)
                     self.chart_data = pd.concat([self.chart_data, new_df], ignore_index=True)
 
                     # Convert 'kg' column to numeric type, handle non-numeric values as NaN
@@ -58,25 +74,32 @@ class SensorWidget(QWidget):
                     # Update the plot curve with the latest data
                     self.plot_curve.setData(valid_data['elapsedTime'], valid_data['kg'])
 
+            else:
+                print('serial port not opened')
 
-            # Add a method to trigger data collection
-            def start_collecting_data():
-                self.start_time = dt.datetime.now()
-                self.timer.timeout.connect(collect_data)
-                self.timer.start(100)
+        # Add a method to trigger data collection
+        def start_collecting_data():
+            self.chart_data = pd.DataFrame()
+            self.start_time = dt.datetime.now()
+            self.timer.timeout.connect(collect_data)
+            self.timer.start(50)
 
-            # Add a method to stop data collection
-            def stop_collecting_data():
-                self.timer.stop()
+        # Add a method to stop data collection
+        def stop_collecting_data():
+            self.timer.stop()
 
-            self.start_collecting_data = start_collecting_data
+            # Save chart data to a CSV file
+            file_name = 'chart_data.csv'
+            self.chart_data.to_csv(file_name, index=False)
+            print(f"Chart data saved to '{file_name}'")
 
-            # Create a button to start data collection
-            self.start_button = QPushButton("Start Collecting Data")
-            self.start_button.clicked.connect(start_collecting_data)
-            self.layout.addWidget(self.start_button)
-            
-            self.stop_button = QPushButton("Stop Collecting Data")
-            self.stop_button.clicked.connect(stop_collecting_data)
-            self.layout.addWidget(self.stop_button)
+        self.start_collecting_data = start_collecting_data
 
+        # Create a button to start data collection
+        self.start_button = QPushButton("Start Collecting Data")
+        self.start_button.clicked.connect(start_collecting_data)
+        self.layout.addWidget(self.start_button)
+
+        self.stop_button = QPushButton("Stop Collecting Data")
+        self.stop_button.clicked.connect(stop_collecting_data)
+        self.layout.addWidget(self.stop_button)
